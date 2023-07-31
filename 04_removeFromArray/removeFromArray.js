5b7a18b1a0d37b7229512beacc8dee277d2fc2b5
Joseph Mudd <josephmuddjr@gmail.com>
2023-07-30 19:31:37 -0500

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(firstArray, ...theArgs) {
+    let array = firstArray
+    ,    args = theArgs;
 
+    for (x of args) {
+        array = array.filter(element => element !== x);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
