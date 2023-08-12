1955752a1c24038ecd49601eb5fdb553dbbfc986
Joseph Mudd <josephmuddjr@gmail.com>
2023-08-12 17:22:10 -0500

    Solve palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    let stringInput = Array.from(string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()); // Regex to remove non-letters and spaces
+
+    let stringReverse = [...stringInput].reverse();
+    let isPalindrome = true;
+
+    console.log(stringInput, stringReverse);
+
+    for (let i = 0; i < stringInput.length; i++) {
+        if (stringInput[i] !== stringReverse[i]) {
+            isPalindrome = false;
+        }
+    }
+
+    return isPalindrome;
 
 };
 
