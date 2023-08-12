e931e88b8b1fa83c6b46011ea7bb4cedb8508d10
Joseph Mudd <josephmuddjr@gmail.com>
2023-08-12 18:44:56 -0500

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = Number(number);
+    if (number < 0) {return "OOPS"};
 
+    let fibArray = [1 , 1];
+
+    while (fibArray.length < number) {
+        fibArray.push((fibArray[fibArray.length - 1]) + (fibArray[fibArray.length - 2]));
+    }
+
+    return fibArray[fibArray.length - 1];
 };
 
 // Do not edit below this line
