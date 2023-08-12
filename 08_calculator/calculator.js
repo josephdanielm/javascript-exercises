a34b38fbf9e0cfe424b32fd3320cd71bac3d2134
Joseph Mudd <josephmuddjr@gmail.com>
2023-08-12 15:57:06 -0500

    Solve calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	return Array.from(numbers).reduce((sum, current) => sum + current);
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+	return Array.from(numbers).reduce((sum, current) => sum - current);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((sum, current) => sum * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {return 1};
+  let factorialArray = [];
+  for (i=number; i<=number && i>0; i--) {
+    factorialArray.push(i);
+  }
+  return factorialArray.reduce((total, current) => total * current);
 };
 
 // Do not edit below this line
