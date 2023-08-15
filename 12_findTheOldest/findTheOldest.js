a099b8ddb6c5233f94c8eed9554782401c4d82db
Joseph Mudd <josephmuddjr@gmail.com>
2023-08-15 13:40:05 -0500

    Solve findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age
+    ,   oldestAge = 0
+    ,   indexOfOldestPerson;
+    for (person of people) {
+        if ('yearOfDeath' in person) {
+            person.age = (person.yearOfDeath - person.yearOfBirth);
+        } else {
+            const today = new Date();
+            person.age = (today.getFullYear() - person.yearOfBirth);
+        }
 
+        if (person.age > oldestAge) {oldestAge = person.age;}
+    }
+
+    return (people.find(person => person.age === oldestAge));
 };
 
 // Do not edit below this line
